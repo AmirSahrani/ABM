6a691d5bbd52f200b88312e0086f4be58e6d7b9f
swatsengels1 <70349220+swatsengels1@users.noreply.github.com>
2024-06-17 12:35:12 +0200

    simple viz

--- scripts/sa_visualizations.py
+++ scripts/sa_visualizations.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('../data/ofat_results.csv')
 
-def plot_ofat_results(df, param_name):
+def plot_ofat_results(df, param_name, output_name):
     df_param = df[df['param'] == param_name]
-    grouped = df_param.groupby('value')['Nomads'].agg(['mean', 'max', 'min']).reset_index()
+    grouped = df_param.groupby('value')[output_name].agg(['mean', 'max', 'min']).reset_index()
     
     plt.figure(figsize=(10, 6))
     for idx, row in grouped.iterrows():
@@ -14,14 +14,14 @@ def plot_ofat_results(df, param_name):
         plt.scatter(row['value'], row['min'], label='Min' if idx == 0 else "", marker='s', color='red')
 
     plt.xlabel(f'{param_name}')
-    plt.ylabel('Number of Nomads')
-    plt.title(f'Mean, Max, and Min Number of Nomads for {param_name}')
+    plt.ylabel(f'{output_name}')
+    plt.title(f'Mean, Max, and Min of {output_name} for {param_name}')
     plt.legend()
     plt.show()
 
 
-# plot_ofat_results(df, 'n_heaps')
-# plot_ofat_results(df, 'vision_radius')
-# plot_ofat_results(df, 'step_count')
-# plot_ofat_results(df, 'alpha')
-# plot_ofat_results(df, 'trade_percentage')
+plot_ofat_results(df, 'n_heaps', 'Cooperation_per_step')
+# plot_ofat_results(df, 'vision_radius', 'Nomads')
+# plot_ofat_results(df, 'step_count', 'Nomads')
+# plot_ofat_results(df, 'alpha', 'Nomads')
+# plot_ofat_results(df, 'trade_percentage', 'Nomads')
