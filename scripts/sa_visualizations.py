a47433f0ba3ab4ccdc827011d1adc1e211480f4e
swatsengels1 <70349220+swatsengels1@users.noreply.github.com>
2024-06-23 17:26:09 +0200

    woof

--- scripts/sa_visualizations.py
+++ scripts/sa_visualizations.py
@@ -25,6 +25,7 @@ def plot_single_ofat_result(df, param_name, output_name):
 #plot_single_ofat_result(df, 'vision_radius', 'Fights_per_step')
 
 def plot_all_ofat_results(df, param_name):
+    print(f'Parameter of Interest: {param_name}')
     metrics = ['Nomads', 'total_Clustering']
     
     fig, axs = plt.subplots(2, 2, figsize=(30, 10))
@@ -32,28 +33,41 @@ def plot_all_ofat_results(df, param_name):
     ax3 = axs[1, 0]
     ax4 = axs[1, 1]
 
+    def get_last_n_indices(group, n=5):
+        return group.iloc[-n:]
+
     # Plotting Nomads and total_Clustering
     for i, metric in enumerate(metrics):
         ax = axs[i // 2, i % 2]
-        grouped = df.groupby(param_name)[metric].agg(['mean', 'max', 'min', 'std']).reset_index()
-        replicates = df.groupby(param_name)[metric].count().reindex(grouped[param_name]).values
-        err = (1.96 * grouped['std']) / np.sqrt(replicates)
-        ax.plot(grouped[param_name], grouped['mean'], label='Mean', color='blue', marker='o')
-        ax.fill_between(grouped[param_name], grouped['mean'] - err, grouped['mean'] + err, color='blue', alpha=0.2)
+        grouped = df.groupby(param_name).apply(lambda x: get_last_n_indices(x)).reset_index(drop=True)
+        grouped_metric = grouped.groupby(param_name)[metric].agg(['mean', 'max', 'min', 'std']).reset_index()
+        replicates = grouped.groupby(param_name)[metric].count().reindex(grouped_metric[param_name]).values
+        err = (1.96 * grouped_metric['std']) / np.sqrt(replicates)
+        ax.plot(grouped_metric[param_name], grouped_metric['mean'], label='Mean', color='blue', marker='o')
+        ax.fill_between(grouped_metric[param_name], grouped_metric['mean'] - err, grouped_metric['mean'] + err, color='blue', alpha=0.2)
         ax.set_xlabel(param_name)
         ax.set_ylabel(metric)
         ax.set_title(f'{metric} vs {param_name}')
-        ax.grid(True) # For Sandor lol, I like em too
+        ax.grid(True)  # For Sandor lol, I like em too
+
+        max_mean_value = grouped_metric['mean'].max()
+        max_param_value = grouped_metric.loc[grouped_metric['mean'].idxmax(), param_name]
+        print(f'Max Parameter Value: {max_param_value}   Max Mean Value: {max_mean_value}')
     
     # Plotting Fights_per_step and Cooperation_per_step
     ax5 = axs[1, 1]
     metrics_to_plot = ['Cooperation_per_step', 'Fights_per_step']
     for metric in metrics_to_plot:
-        grouped = df.groupby(param_name)[metric].agg(['mean', 'max', 'min', 'std']).reset_index()
-        replicates = df.groupby(param_name)[metric].count().reindex(grouped[param_name]).values
-        err = (1.96 * grouped['std']) / np.sqrt(replicates)
-        ax5.plot(grouped[param_name], grouped['mean'], label=metric, linestyle='-', marker='o')
-        ax5.fill_between(grouped[param_name], grouped['mean'] - err, grouped['mean'] + err, alpha=0.2)
+        grouped = df.groupby(param_name).apply(lambda x: get_last_n_indices(x)).reset_index(drop=True)
+        grouped_metric = grouped.groupby(param_name)[metric].agg(['mean', 'max', 'min', 'std']).reset_index()
+        replicates = grouped.groupby(param_name)[metric].count().reindex(grouped_metric[param_name]).values
+        err = (1.96 * grouped_metric['std']) / np.sqrt(replicates)
+        ax5.plot(grouped_metric[param_name], grouped_metric['mean'], label=metric, linestyle='-', marker='o')
+        ax5.fill_between(grouped_metric[param_name], grouped_metric['mean'] - err, grouped_metric['mean'] + err, alpha=0.2)
+        
+        max_mean_value = grouped_metric['mean'].max()
+        max_param_value = grouped_metric.loc[grouped_metric['mean'].idxmax(), param_name]
+        print(f'Max Parameter Value: {max_param_value}   Max Mean Value: {max_mean_value}')
 
     ax5.set_xlabel(param_name)
     ax5.set_ylabel('Cooperation_per_step')
@@ -64,11 +78,12 @@ def plot_all_ofat_results(df, param_name):
     # Plotting average trades across all tribes
     trades_columns = ['Tribe_{}_Trades'.format(i) for i in range(4)]
     df['average_trades'] = df[trades_columns].mean(axis=1)
-    grouped = df.groupby(param_name)['average_trades'].agg(['mean', 'max', 'min', 'std']).reset_index()
-    replicates = df.groupby(param_name)['average_trades'].count().reindex(grouped[param_name]).values
-    err = (1.96 * grouped['std']) / np.sqrt(replicates)
-    ax3.plot(grouped[param_name], grouped['mean'], label='Average Trades', color='purple', marker='o')
-    ax3.fill_between(grouped[param_name], grouped['mean'] - err, grouped['mean'] + err, color='purple', alpha=0.2)
+    grouped = df.groupby(param_name).apply(lambda x: get_last_n_indices(x)).reset_index(drop=True)
+    grouped_metric = grouped.groupby(param_name)['average_trades'].agg(['mean', 'max', 'min', 'std']).reset_index()
+    replicates = grouped.groupby(param_name)['average_trades'].count().reindex(grouped_metric[param_name]).values
+    err = (1.96 * grouped_metric['std']) / np.sqrt(replicates)
+    ax3.plot(grouped_metric[param_name], grouped_metric['mean'], label='Average Trades', color='purple', marker='o')
+    ax3.fill_between(grouped_metric[param_name], grouped_metric['mean'] - err, grouped_metric['mean'] + err, color='purple', alpha=0.2)
     ax3.set_xlabel(param_name)
     ax3.set_ylabel('Average Trades')
     ax3.set_title(f'Average Trades vs {param_name}')
@@ -78,7 +93,7 @@ def plot_all_ofat_results(df, param_name):
     plt.tight_layout()
     plt.subplots_adjust(left=0.046, bottom=0.268, right=0.988, top=0.964, wspace=0.129, hspace=0.23)
     plt.show()
-
+    
 
 plot_all_ofat_results(df, 'n_tribes')
 plot_all_ofat_results(df, 'n_agents')
