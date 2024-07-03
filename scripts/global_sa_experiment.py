08603dd64d5a34a2ee3b4f5bd6230ca2787567eb
AmirSahrani <amirsahrani1@gmail.com>
2024-07-03 19:40:59 +0200

    fixes

--- scripts/global_sa_experiment.py
+++ scripts/global_sa_experiment.py
@@ -16,14 +16,16 @@ sys.path.remove("../scripts")
 
 step_count = 300
 frequency = 50
-NUM_SAMPLES = step_count//frequency
+NUM_SAMPLES = step_count // frequency
+
+
 def sensitivity_target(inp):
     '''
     Wrapper for the model, averages over all the possible maps we have defined, cov_range, total_spice,spice_grow_threshold, and step_count are hard coded. total_spice is 0.2*width*height of the grid. 
     '''
-    random_spice_map_picker = lambda *args: np.random.choice([gen_spice_map, gen_central_spice_heap])( *args)
-    random_river_picker = lambda *args: np.random.choice([gen_river_random, gen_river_line, no_river])( *args)
-    random_location_picker = lambda *args: np.random.choice([random_locations, split_tribes_locations, tribe_locations_naturally_distributed, tribe_locations_single_cluster_per_tribe])( *args)
+    random_spice_map_picker = lambda *args: np.random.choice([gen_spice_map, gen_central_spice_heap])(*args)
+    random_river_picker = lambda *args: np.random.choice([gen_river_random, gen_river_line, no_river])(*args)
+    random_location_picker = lambda *args: np.random.choice([random_locations, split_tribes_locations, tribe_locations_naturally_distributed, tribe_locations_single_cluster_per_tribe])(*args)
     kwargs = {
         "experiment_name": "test",
         "spice_generator": random_spice_map_picker,
@@ -37,13 +39,13 @@ def sensitivity_target(inp):
             width=width,
             height=height,
             n_tribes=int(inp[0]),
-            n_agents=int(width*height/10),
+            n_agents=int(width * height / 10),
             n_heaps=int(inp[1]),
-            vision_radius=int(inp[2]), step_count=step_count, 
+            vision_radius=int(inp[2]), step_count=step_count,
             trade_percentage=float(inp[3]),
             spice_movement_bias=float(inp[4]),
             tribe_movement_bias=float(inp[5]),
-            spice_threshold=float(inp[6]*kwargs["spice_kwargs"]["total_spice"]),
+            spice_threshold=float(inp[6] * kwargs["spice_kwargs"]["total_spice"]),
             spice_grow_threshold=20,
             frequency=frequency,
             **kwargs
@@ -55,16 +57,15 @@ def sensitivity_target(inp):
         raise e
 
 
-
 def parallel_evaluation(fun, samples, user, n_jobs=None):
     '''
     Evaluate the model on the samples in parallel.
-    
+
     Parameters:
     fun (Callable): The function to be evaluated.
     samples (iterable): An iterable of samples to be passed to the function.
     n_jobs (int, optional): The number of jobs to run in parallel. Defaults to the number of CPU cores.
-    
+
     Returns:
     np.ndarray [len(samples), len(output_parameters), 10]: A numpy array with evaluation results.
     '''
@@ -76,34 +77,31 @@ def parallel_evaluation(fun, samples, user, n_jobs=None):
         "Fights_per_step",
         "Cooperation_per_step"
     ]
-    
+
     batch_size = 32
-    
+
     out = np.empty((len(samples), len(output_params), NUM_SAMPLES))
     results = []
-    mid =  np.empty((len(samples), len(output_params), NUM_SAMPLES))
-
+    mid = np.empty((len(samples), len(output_params), NUM_SAMPLES))
 
     # Use joblib's Parallel and delayed for multiprocessing
     print(samples.shape)
     for batch in range(0, samples.shape[0], batch_size):
         print(f'Starting with batch: {batch//batch_size}, {(batch)/samples.shape[0] * 100:.2f}% done')
-        batch_results = Parallel(n_jobs=n_jobs, verbose=0, backend='multiprocessing')(delayed(fun)(sample) for sample in samples[batch:batch+batch_size, :])
-        
+        batch_results = Parallel(n_jobs=n_jobs, verbose=0, backend='multiprocessing')(delayed(fun)(sample) for sample in samples[batch:batch + batch_size, :])
+
         for i, result in enumerate(batch_results, start=batch):
             for j, param in enumerate(output_params):
                 mid[i, j, :] = result[param][-NUM_SAMPLES:]
-        
+
         with open(f'GSA/intermediate_{user}.npy', 'wb') as f:
-           np.save(f, mid)
-        
-        
+            np.save(f, mid)
 
     for i, result in enumerate(results):
         for j, param in enumerate(output_params):
             out[i, j, :] = result[param][-NUM_SAMPLES:]
     with open(f'GSA/final_{user}.npy', 'wb') as f:
-       np.save(f, out)
+        np.save(f, out)
 
     return out
 
@@ -151,9 +149,9 @@ def main(data_prov):
     samples = pd.read_csv('GSA/sobol_samples_1024.csv')
 
     # 1024*(7+2) = 9216, 9216/2 = 4608, so I generate 75%
-    samples, user = samples.iloc[0: 4608], 'amir_pc' # AMIR COMPUTER DATA
-    # samples, user = samples.iloc [4608: (4608 + 4608/2)] 'amir_laptop' # AMIR LAPTOP DATA
-    # samples, user = samples.iloc [(4608 + 4608/2):] 'sandor' # SANDOR DATA
+    # samples, user = samples.iloc[0: 4608], 'amir_pc' # AMIR COMPUTER DATA
+    samples, user = samples.iloc[4608: int(4608 + 4608 / 2)], 'amir_laptop'  # AMIR LAPTOP DATA
+    # samples, user = samples.iloc [int(4608 + 4608/2):], 'sandor' # SANDOR DATA
     samples = samples.to_numpy()
 
     if not data_prov:
@@ -183,7 +181,6 @@ def main(data_prov):
                 'ST': Si['ST'],
                 'ST_conf': Si['ST_conf']
             }
-
 
             sobol_indices_df = pd.DataFrame(sobol_indices_data)
             sobol_indices_df.to_csv(os.path.join(output_dir, f'sobol_results_{output_params[param]}_sample_{nr_sobol_samples}_step_{(step_count+1)*frequency}.csv'), index=False)
